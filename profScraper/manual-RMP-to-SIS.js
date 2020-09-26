3a826fdf4730d0b916ca1e6c14425248df413693
SamuraiViking <nelson67@stolaf.edu>
2020-09-25 21:15:08 -0500

    updated profs

--- profScraper/manual-RMP-to-SIS.js
+++ profScraper/manual-RMP-to-SIS.js
@@ -5,8 +5,15 @@ const MANUAL_RMP_TO_SIS = {
     // "Chapp Chris": "Chapp, Christopher B.",
     "furnstein michael": "Fuerstein, Michael",
     "Mahr Tim": "Mahr, Timothy",
+    "Vonbibra Anne": "von Bibra,",
+    "Javadi Nooshin": "Hakim Javadi,",
     // "Schodt David": "Schalliol, David C.",
+    "Huff Chuck": "Huff, Charles",
     "Nesbit Coach": "Nesbit, Scott",
+    "Wong KA": "Wong, Ka",
+    "Atiyat Al Ibtesam": "Al Atiyat,",
+    "II Rainey Timothy": "Rainey, Timothy M. II.",
+    "Cosimini Hill Amy": "Hill Cosimini, Amy M.",
     // "Wong KA": "Wong, Ka F.",
 };
 
