6b57a5c4478e666ca205283c903990982df3ad72
SamuraiViking <nelson67@stolaf.edu>
2020-09-28 13:00:17 -0500

    works

--- extension/content.js
+++ extension/content.js
@@ -90,9 +90,11 @@ const insertProfessorRatings = (RMPProfs) => {
         }
     );
 
+    console.log(profElements)
+
     for (let i = 0; i < profElements.length; i++) {
         let professor = profElements[i].innerText;
-        professor = formatProfessor(professor);
+        // professor = formatProfessor(professor);
         const RMPProf = RMPProfs[professor]
         insertProfRating(profElements[i], RMPProf, professor)
     }
